e61189015a70690829077db2520698079109cd9e
projesh-BVS <144583856+projesh-BVS@users.noreply.github.com>
2024-08-29 07:02:16 +0000

    Auto Multiplier Settings added

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -27,109 +27,109 @@ export const SidebarData = [
     icon: password,
     cName: "sub-nav",
   },
-  // {
-  //   title: "Profile",
-  //   path: "#",
-  //   icon: <AiIcons.AiFillHome />,
-  //   iconClosed: <RiIcons.RiArrowDownSFill />,
-  //   iconOpened: <RiIcons.RiArrowUpSFill />,
+  /*{
+    title: "Profile",
+    path: "#",
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
 
-  //   subNav: [
-  //     {
-  //       title: "Change Username",
-  //       path: "/profile/username",
-  //       icon: <IoIcons.IoIosPaper />,
-  //     },
-  //     {
-  //       title: "Change Passowrd",
-  //       path: "/profile/changepassword",
-  //       icon: <IoIcons.IoIosPaper />,
-  //     },
-  //   ],
-  // },
-  // {
-  //   title: 'Admin',
-  //   icon: <FaIcons.FaCartPlus />,
-  //   iconClosed: <RiIcons.RiArrowDownSFill />,
-  //   iconOpened: <RiIcons.RiArrowUpSFill />,
-  //   subNav: [
-  //       {
-  //         title: 'Add Admin',
-  //         path: '/regAdmin',
-  //         icon: <IoIcons.IoIosPaper />,
-  //         cName: 'sub-nav'
-  //       },
-  //       {
-  //         title: ' List Admin',
-  //         path: '/listAdmin',
-  //         icon: <AiIcons.AiOutlineBars />,
-  //         cName:'sub-nav'
-  //       },
-  //   ]
-  //   },
-  // {
-  //   title: 'Distributor',
-  //   icon: <IoIcons.IoIosPaper />,
-  //   iconClosed: <RiIcons.RiArrowDownSFill />,
-  //   iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: "Change Username",
+        path: "/profile/username",
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: "Change Passowrd",
+        path: "/profile/changepassword",
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: 'Admin',
+    icon: <FaIcons.FaCartPlus />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+        {
+          title: 'Add Admin',
+          path: '/regAdmin',
+          icon: <IoIcons.IoIosPaper />,
+          cName: 'sub-nav'
+        },
+        {
+          title: ' List Admin',
+          path: '/listAdmin',
+          icon: <AiIcons.AiOutlineBars />,
+          cName:'sub-nav'
+        },
+    ]
+    },
+  {
+    title: 'Distributor',
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
 
-  //   subNav: [
-  //     {
-  //       title: 'List Distributor',
-  //       path: '/distributor/list',
-  //       icon: <AiIcons.AiOutlineBars />,
-  //       cName: 'sub-nav'
-  //     },
-  //     {
-  //       title: 'Add Distributor ',
-  //       path: '/distributor/create',
-  //       icon: <IoIcons.IoIosPaper />,
-  //       cName: 'sub-nav'
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: 'Stokist',
-  //   icon: <IoIcons.IoIosPaper />,
-  //   iconClosed: <RiIcons.RiArrowDownSFill />,
-  //   iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: 'List Distributor',
+        path: '/distributor/list',
+        icon: <AiIcons.AiOutlineBars />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Add Distributor ',
+        path: '/distributor/create',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+    ]
+  },
+  {
+    title: 'Stokist',
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
 
-  //   subNav: [
-  //     {
-  //       title: 'List Stokist',
-  //       path: '/stokist/list',
-  //       icon: <AiIcons.AiOutlineBars />,
-  //       cName: 'sub-nav'
-  //     },
-  //     {
-  //       title: 'Add Stokist ',
-  //       path: '/stokist/create',
-  //       icon: <IoIcons.IoIosPaper />,
-  //       cName: 'sub-nav'
-  //     },
-  //   ]
-  // },
-  // {
-  //   title: 'Player',
-  //   icon: <IoIcons.IoIosPaper />,
-  //   iconClosed: <RiIcons.RiArrowDownSFill />,
-  //   iconOpened: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: 'List Stokist',
+        path: '/stokist/list',
+        icon: <AiIcons.AiOutlineBars />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Add Stokist ',
+        path: '/stokist/create',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+    ]
+  },
+  {
+    title: 'Player',
+    icon: <IoIcons.IoIosPaper />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
 
-  //   subNav: [
-  //     {
-  //       title: 'List Player',
-  //       path: '/player/list',
-  //       icon: <AiIcons.AiOutlineBars />,
-  //       cName: 'sub-nav'
-  //     },
-  //     {
-  //       title: 'Add Player ',
-  //       path: '/player/create',
-  //       icon: <IoIcons.IoIosPaper />,
-  //       cName: 'sub-nav'
-  //     },
-  //   ]
-  // },
+    subNav: [
+      {
+        title: 'List Player',
+        path: '/player/list',
+        icon: <AiIcons.AiOutlineBars />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Add Player ',
+        path: '/player/create',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'sub-nav'
+      },
+    ]
+  },*/
   {
     title: "User Manager",
     path: "/userManager",
--- src/pages/GameSettings.js
+++ src/pages/GameSettings.js
@@ -19,6 +19,8 @@ import RandomResult from "../components/RandomResult";
 import { getCookie, getRole } from "../utils/auth";
 
 const GameSettings = () => {
+  const currHour = new Date().getHours();
+
   const [gameType, setGameType] = useState("All")
   const [multiplier, setMultiplier] = useState("")
   const [payoutPercent, setpayoutPercent] = useState('')
@@ -26,6 +28,42 @@ const GameSettings = () => {
   const [loggedUserRole, setloggedUserRole] = useState('')
   const [supAdminMultiplier, setSupAdminMultiplier] = useState([])
   const [adminMultiplier, setAdminMultiplier] = useState([])
+
+  const [mult_GameType, setMult_GameType] = useState("cards16");
+  const [mult_ExpHour, setMult_ExpHour] = useState(currHour + 1);
+  const [mult_MultiplierAmt, setMult_MultiplierAmt] = useState("");
+  const [mult_HowManyTimes, setMult_HowManyTimes] = useState("");
+
+  // Array of hours from 0 to 23
+  const hoursArray = Array.from({ length: 24 }, (_, i) => i);
+
+  // Convert 24-hour time to 12-hour time with AM/PM
+  const formatHour = (hour) => {
+    const period = hour >= 12 ? "PM" : "AM";
+    const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
+    return `${formattedHour}:00 ${period}`;
+  };
+
+  // Calculate the duration from the current hour
+  const calculateDuration = (hour) => {
+    const now = new Date();
+    const targetTime = new Date();
+  
+    targetTime.setHours(hour, 0, 0, 0);
+  
+    const durationInMs = targetTime - now;
+  
+    // If the target time is before the current time, add 24 hours to get the correct duration
+    if (durationInMs < 0) {
+      targetTime.setDate(now.getDate() + 1);
+    }
+  
+    const durationInMsUpdated = targetTime - now;
+    const hours = Math.floor(durationInMsUpdated / (1000 * 60 * 60));
+    const minutes = Math.floor((durationInMsUpdated % (1000 * 60 * 60)) / (1000 * 60));
+  
+    return `${hours > 0 ? `${hours} hr ` : ""}${minutes} min`;
+  };  
 
   useEffect(() => {
     const getPayoutPercent = async () => {
@@ -175,6 +213,44 @@ const GameSettings = () => {
     }
   };
 
+  const handleSubmitAutoMultiplier = async (e) => {
+    e.preventDefault();
+
+    if ((1 > mult_MultiplierAmt) || (mult_MultiplierAmt > 20)) {
+      alert("Enter Multiplier in range 1-20");
+      return;
+    }
+
+    if ((1 > mult_HowManyTimes) || (mult_HowManyTimes > 20)) {
+      alert("Enter How Many Times in range 1-20");
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        `https://gf8mf58fp2.execute-api.ap-south-1.amazonaws.com/Royal_prod/automultiplier?userID=${loggedUser}`,
+        {
+          adminID: loggedUser,
+          gameType: mult_GameType,
+          hour: mult_ExpHour,
+          howManyLeft: mult_HowManyTimes,
+          multiplier: mult_MultiplierAmt,
+        }
+      );
+      // console.log(res)
+      // console.log(gameType, payoutPercent)
+
+      setMult_ExpHour(currHour + 1);
+      setMult_GameType("cards16");
+      setMult_HowManyTimes("");
+      setMult_MultiplierAmt("");
+
+      toast.success('Auto Multiplier successfully saved')
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <Container>
@@ -198,9 +274,9 @@ const GameSettings = () => {
                     <option value="All">All ⇨ Current payout percentage {allPayoutPercents.all}%</option>
                     <option value="cards16">Cards 16 ⇨ Current payout percentage {allPayoutPercents.card16}%</option>
                     <option value="cards52">Cards 52 ⇨ Current payout percentage {allPayoutPercents.card52}%</option>
-                    <option value="jeetoJoker">jeetoJoker ⇨ Current payout percentage {allPayoutPercents.jeetoJoker}%</option>
-                    <option value="doubleChance">doubleChance ⇨ Current payout percentage {allPayoutPercents.doubleChance}%</option>
-                    <option value="singleChance">signleChance ⇨ Current payout percentage {allPayoutPercents.singleChance}%</option>
+                    <option value="jeetoJoker">Jeeto Joker ⇨ Current payout percentage {allPayoutPercents.jeetoJoker}%</option>
+                    <option value="doubleChance">Double Chance ⇨ Current payout percentage {allPayoutPercents.doubleChance}%</option>
+                    <option value="singleChance">Single Chance ⇨ Current payout percentage {allPayoutPercents.singleChance}%</option>
                     <option value="cards24">Cards 24 ⇨ Current payout percentage {allPayoutPercents.card24}%</option>
                   </Form.Select>
                 </FloatingLabel>
@@ -280,6 +356,100 @@ const GameSettings = () => {
                   Submit
                 </Button>
               </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+
+        <Card className="mt-4 w-100 shadow-lg">
+          <Card.Header>
+            <h4 className="text-center">Auto Multiplier Settings</h4>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title className="text-muted">Configure Settings Below</Card.Title>
+            <br />
+
+            <Row className="g-2">
+              {/* Game Type Dropdown */}
+              <Col md>
+                <FloatingLabel controlId="floatingSelectGrid" label="Select Game Type">
+                  <Form.Select
+                    onChange={(e) => setMult_GameType(e.target.value)}
+                    aria-label="Floating label select example"
+                    value={mult_GameType}
+                    required
+                  >                    
+                    <option value="cards16">Cards 16</option>
+                    <option value="cards52">Cards 52</option>
+                    <option value="jeetoJoker">Jeeto Joker</option>
+                    <option value="doubleChance">Double Chance</option>
+                    <option value="singleChance">Single Chance</option>
+                    <option value="cards24">Cards 24</option>
+                  </Form.Select>
+                </FloatingLabel>
+              </Col>
+
+              {/* Hour Dropdown */}
+              <Col md>
+                <FloatingLabel controlId="floatingSelectGrid" label="Select Duration">
+                  <Form.Select
+                    onChange={(e) => setMult_ExpHour(e.target.value)}
+                    aria-label="Floating label select example"
+                    value={mult_ExpHour}
+                  >
+                    {hoursArray.map((hour) => (
+                      <option key={hour} value={hour}>
+                        Till {formatHour(hour)} | For {calculateDuration(hour)}
+                      </option>
+                    ))}
+                  </Form.Select>
+                </FloatingLabel>
+              </Col>
+
+              {/* Multiplier Amount */}
+              <Col md>
+                <FloatingLabel
+                  controlId="floatingSelectGrid"
+                  label="Enter Multiplier (1-20)"
+                >
+                  <Form.Control
+                    onChange={((e) => setMult_MultiplierAmt(e.target.value))}
+                    type="number"
+                    min={1} max={20}
+                    placeholder="Enter values from 1 to 20"
+                    value={mult_MultiplierAmt}
+                    required={true}
+                  />
+                </FloatingLabel>
+              </Col>
+
+              {/* How Many Dropdown */}              
+              <Col md>
+                <FloatingLabel
+                  controlId="floatingSelectGrid"
+                  label="Enter How Many Times (1-20)"
+                >
+                  <Form.Control
+                    onChange={((e) => setMult_HowManyTimes(e.target.value))}
+                    type="number"
+                    min={1} max={20}
+                    placeholder="Enter values from 1 to 20"
+                    value={mult_HowManyTimes}
+                    required={true}
+                  />
+                </FloatingLabel>
+              </Col>                 
+            </Row>
+
+            <Row className="g-2">
+              <Col md>
+                <Button
+                  className="float-right mr-4 mt-4"
+                  onClick={handleSubmitAutoMultiplier}
+                  variant="secondary"
+                >
+                  Submit
+                </Button>
+              </Col>              
             </Row>
           </Card.Body>
         </Card>
